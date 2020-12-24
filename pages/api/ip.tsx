b1f1327f1c442d40d00a0f70a601954453fc52f4
sedgwickz <kunnsh@gmail.com>
2020-12-24 09:23:10 +0800

    add eslint & prettier

---
+++ pages/api/ip.tsx
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        res.statusCode = 200
+        const resp = await fetch(
+            'http://qqwry.vercel.app/api?ip=' + req.query.ip
+        )
+        const json = await resp.json()
+        res.json(json)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
